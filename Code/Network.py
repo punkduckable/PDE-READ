07ec103c3af866b3b08ffb8c4fa4cdfc176df86d
Robert Stephany <robert.stephany@utexas.edu>
2021-05-12 19:05:16 -0400

    Updated the Network class
    I didn't change anything major. I mostly just modernized the comments
    and renamed a few variables. I plan to make bigger changes (such as
    passing an entire batch of samples through the NN objects at once) in
    the next commit.

--- Code/Network.py
+++ Code/Network.py
@@ -5,14 +5,14 @@ import torch;
 
 class Neural_Network(torch.nn.Module):
     def __init__(self,
-                 Num_Hidden_Layers : int = 3,    # Number of Hidden Layers
-                 Nodes_Per_Layer : int   = 20,   # Nodes in each Hidden Layer
-                 Input_Dim : int         = 1,    # Number of components in the input
-                 Output_Dim : int        = 1):   # Number of components in the output
-        # Note: we assume that Num_Hidden_Layers, Nodes_Per_Layer, Input_Dim,
+                 Num_Hidden_Layers : int = 3,
+                 Neurons_Per_Layer : int = 20,   # Neurons in each Hidden Layer
+                 Input_Dim : int         = 1,    # Dimension of the input
+                 Output_Dim : int        = 1):   # Dimension of the output
+        # Note: we assume that Num_Hidden_Layers, Neurons_Per_Layer, Input_Dim,
         # and out_dim are positive integers.
         assert (Num_Hidden_Layers > 0   and
-                Nodes_Per_Layer > 0     and
+                Neurons_Per_Layer > 0   and
                 Input_Dim > 0           and
                 Output_Dim > 0), "Neural_Network initialization arguments must be positive integers!"
 
@@ -31,28 +31,28 @@ class Neural_Network(torch.nn.Module):
 
         # Append the first hidden layer. The domain of this layer is the input
         # domain, which means that in_features = Input_Dim. Since this is a
-        # hidden layer, however it must have Nodes_Per_Layer output features.
+        # hidden layer, however it must have Neurons_Per_Layer output features.
         self.Layers.append(
             torch.nn.Linear(    in_features  = Input_Dim,
-                                out_features = Nodes_Per_Layer,
+                                out_features = Neurons_Per_Layer,
                                 bias = True )
         );
 
         # Now append the rest of the hidden layers. Each of these layers maps
-        # within the same space, which means that in_features = out_features.
-        # Note that we start at i = 1 because we already made the 1st
-        # hidden layer.
+        # from \mathbb{R}^{Neurons_Per_Layer} to itself. Thus, in_features =
+        # out_features = Neurons_Per_Layer. We start at i = 1 because we already
+        # setup the 1st hidden layer.
         for i in range(1, Num_Hidden_Layers):
             self.Layers.append(
-                torch.nn.Linear(    in_features  = Nodes_Per_Layer,
-                                    out_features = Nodes_Per_Layer,
+                torch.nn.Linear(    in_features  = Neurons_Per_Layer,
+                                    out_features = Neurons_Per_Layer,
                                     bias = True )
             );
 
-        # Now, append the Output Layer, which has Nodes_Per_Layer input
+        # Now, append the Output Layer, which has Neurons_Per_Layer input
         # features, but only Output_Dim output features.
         self.Layers.append(
-            torch.nn.Linear(    in_features  = Nodes_Per_Layer,
+            torch.nn.Linear(    in_features  = Neurons_Per_Layer,
                                 out_features = Output_Dim,
                                 bias = True )
         );
@@ -67,7 +67,17 @@ class Neural_Network(torch.nn.Module):
 
 
     def forward(self, x : torch.Tensor) -> torch.Tensor:
-        # Note: the input must be an Input_Dim dimensional (1d) tensor.
+        """ Forward method for the NN class (to enable calling the network).
+        Note that the user should not call this function directly. Rather,
+        they should call it through the __call__ method (using the NN object
+        like a function), which is defined in the module class and calls
+        forward.
+
+        ------------------------------------------------------------------------
+        Arguments:
+        x : A batch of inputs. This should be a B by Input_Dim tensor, whose
+        ith row holds the ith input (this is how the Linear function works).
+        """
 
         # Pass x through the network's layers!
         for i in range(self.Num_Layers - 1):
